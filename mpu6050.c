7baff118bad1ca7facf6679c8f94c59d32b5db9a
ubIQio <ubIQio@users.noreply.github.com>
2017-09-26 07:14:42 +0100

    Change to MPU9255  product code

--- mpu6050.c
+++ mpu6050.c
@@ -8,15 +8,15 @@ int MPU6050_Test_I2C(int handle)
 {
     unsigned char Data = 0x00;
     Data = I2CWrapperReadByte(handle, MPU6050_RA_WHO_AM_I);
- 
-    if(Data == 0x68)
+
+    if(Data == 0x73)
     {
-      printf("I2C Read Test Passed\r\n");       
-      return(1);   
+      printf("I2C Read Test Passed\r\n");
+      return(1);
    }
-     
+
     printf("ERROR: I2C Read Test Failed, returned=%X Stopping\r\n",Data);
- 
+
     return(0);
 }
 
@@ -25,7 +25,7 @@ void Setup_MPU6050(int handle)
 {
    int loop;
    unsigned char TheReg;
-  
+
 // I need code space. Just create a table with register to clear
 const unsigned char MPU6050RegTable[]= {
     MPU6050_RA_FF_THR,    		//Freefall threshold of |0mg|  LDByteWriteI2C(MPU6050_ADDRESS, MPU6050_RA_FF_THR, 0x00);
@@ -101,7 +101,7 @@ const unsigned char MPU6050RegTable[]= {
     MPU6050_RA_INT_ENABLE,    //Enable data ready interrupt    LDByteWriteI2C(MPU6050_ADDRESS, MPU6050_RA_INT_ENABLE, 0x00);
     MPU6050_RA_FIFO_R_W,       // LDByteWriteI2C(MPU6050_ADDRESS, MPU6050_RA_FIFO_R_W, 0x00);
     0xff
-};   
+};
 
 
 
@@ -128,7 +128,7 @@ const unsigned char MPU6050RegTable[]= {
     I2CWrapperWriteByte(handle, MPU6050_RA_PWR_MGMT_1, 0b00000010);
     //Controls frequency of wakeups in accel low power mode plus the sensor standby modes
     I2CWrapperWriteByte(handle, MPU6050_RA_PWR_MGMT_2, 0x00);
- 
+
     //MPU6050_RA_WHO_AM_I             //Read-only, I2C address
     I2CWrapperWriteByte(handle, MPU6050_RA_INT_ENABLE, 0x01);
   printf("\r\nMPU6050 Setup Complete\r\n");
@@ -149,7 +149,7 @@ char cval[14];
 	GData->Gyrox = ((cval[8]<<8)|cval[9]);
 	GData->Gyroy = ((cval[10]<<8)|cval[11]);
 	GData->Gyroz = ((cval[12]<<8)|cval[13]);
-        
+
 }
 
 
@@ -164,8 +164,4 @@ unsigned char GotInt_MPU6050(int handle)
 	uc_temp= I2CWrapperReadByte(handle,MPU6050_RA_INT_STATUS);
 
   return ((uc_temp & 1) == 1 ? 1 : 0);
-}	
-
-
-
-
+}
